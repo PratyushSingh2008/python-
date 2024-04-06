555ea1df02fb387a606e7b838497e2dbe9568549
PratyushSingh2008 <166203722+PratyushSingh2008@users.noreply.github.com>
2024-04-06 12:17:00 +0530

    Add files via upload

---
+++ swapping values.py
@@ -0,0 +1,11 @@
+a = int(input("no one "))
+c = int(input("no two "))
+d = int(input("no three "))
+if a>c and a>d:
+        print("a is large")
+elif c>a and c>d:
+        print("C is large")
+elif a==c==d:
+        print("all are equal")
+else:
+        print("D is large")
